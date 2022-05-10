80db9080983221549945a8ae46b58d3cf32bb4ab
Seiya Omori <forest.sh90830@gmail.com>
2022-05-10 23:04:32 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const CONTRACT_ADDRESS = "0xCCa44fb2B9E072F2C9025e031931E9BD721492e0";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
